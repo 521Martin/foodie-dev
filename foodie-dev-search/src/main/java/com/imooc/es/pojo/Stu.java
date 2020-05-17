58186ebd36fefa6d5db8a7edc761ade7137a71c5
Martin <jielunxiaolove@163.com>
2020-05-17 17:57:46 +0800

    初始版本

---
+++ foodie-dev-search/src/main/java/com/imooc/es/pojo/Stu.java
@@ -0,0 +1,94 @@
+package com.imooc.es.pojo;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+/**
+ * ClassName:Stu
+ * Author:   Martin
+ * Date:     2020/5/13 23:53
+ * Description:
+ */
+@Document(indexName = "stu",type = "_doc",shards = 3,replicas = 0)
+public class Stu {
+
+    @Id
+    private Long stuId;
+
+    @Field(store = true)
+    private String name;
+
+    @Field(store = true)
+    private Integer age;
+
+    @Field(store = true)
+    private float money;
+
+    @Field(store = true,type = FieldType.Keyword)
+    private String sign;
+
+    @Field(store = true)
+    private String description;
+
+    public Long getStuId() {
+        return stuId;
+    }
+
+    public void setStuId(Long stuId) {
+        this.stuId = stuId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public float getMoney() {
+        return money;
+    }
+
+    public void setMoney(float money) {
+        this.money = money;
+    }
+
+    public String getSign() {
+        return sign;
+    }
+
+    public void setSign(String sign) {
+        this.sign = sign;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Stu{" +
+                "stuId=" + stuId +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", money=" + money +
+                ", sign='" + sign + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
